6f32a5bfd8cd2a907488a10f106b7f0a42282623
SarkisChohmalian <sarkis.chohmalian@gmail.com>
2024-03-18 19:39:16 +0400

    Bottom Navigation bar is functional and problems with dark mode are resolved.

--- app/src/main/java/com/example/mathhub/MainActivity.java
+++ app/src/main/java/com/example/mathhub/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.mathhub;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.mathhub.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home) {
+                    replaceFragment(new HomeFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.weeklyprob) {
+                    replaceFragment(new WeeklyprobFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.calculator) {
+                    replaceFragment(new CalculatorFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.learn) {
+                    replaceFragment(new LearnFragment());
+                    return true;
+                }
+                return false;
+            }
         });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
