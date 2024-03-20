da42ca421df6e4d1913238695cc8ec69f868d37e
SarkisChohmalian <sarkis.chohmalian@gmail.com>
2024-03-20 21:23:09 +0400

    Settings is semi functional

--- app/src/main/java/com/example/mathhub/SettingsActivity.java
+++ app/src/main/java/com/example/mathhub/SettingsActivity.java
@@ -1,24 +1,80 @@
 package com.example.mathhub;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView changeUsernameTextView = findViewById(R.id.change_username);
+        TextView changePasswordTextView = findViewById(R.id.change_password);
+        TextView policyTextView = findViewById(R.id.policy);
+        TextView faqTextView = findViewById(R.id.FAQ);
+        TextView logOutTextView = findViewById(R.id.log_out);
+        TextView deleteAccountTextView = findViewById(R.id.Delete_account);
+        ImageView backImageView = findViewById(R.id.back);
+
+        backImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        changeUsernameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, ChangeUsernameActivity.class));
+            }
+        });
+
+        changePasswordTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, PasswordResetActivity.class));
+            }
+        });
+
+        policyTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, PolicyActivity.class));
+            }
+        });
+
+        faqTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, FAQActivity.class));
+            }
+        });
+
+        logOutTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getSharedPreferences("loginPrefs", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("rememberMe", false).apply();
+                startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+        deleteAccountTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, DeleteAccountActivity.class));
+            }
         });
     }
 }
