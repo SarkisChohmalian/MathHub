926af6898fd1363bfec2592729efadbdc225168b
SarkisChohmalian <sarkis.chohmalian@gmail.com>
2024-03-22 22:35:32 +0400

    Now Create post design is almost ready only java code and 10% design left

--- app/src/main/java/com/example/mathhub/MainActivity.java
+++ app/src/main/java/com/example/mathhub/MainActivity.java
@@ -6,11 +6,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.mathhub.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -47,15 +45,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         binding.createpost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreatePostsActivity.class));
+                // Create an instance of the CreatePostFragment
+                CreatePostFragment createPostFragment = new CreatePostFragment();
+
+                // Replace the current fragment with the CreatePostFragment
+                replaceFragment(createPostFragment);
             }
         });
-
-
     }
 
     private void replaceFragment(Fragment fragment) {
